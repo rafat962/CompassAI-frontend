2137c015a37df3bf591e867e925b00fae62ad7e4
rafat962 <rafatkamel96@gmail.com>
2025-10-14 11:17:26 +0300

    Share-module

--- src/components/CompassAI-V3/Chat/utils/ChatBody/SystemMessage.jsx
+++ src/components/CompassAI-V3/Chat/utils/ChatBody/SystemMessage.jsx
@@ -7,7 +7,7 @@ const SystemMessage = ({ item, selectField }) => {
             <div className="flex items-center shadow-md ring-2 ring-sky-300 select-none cursor-pointer justify-center rounded-full px-6 py-1 ">
                 <h1 className="text-md font-sec">
                     Available Fields ({" "}
-                    <span className="text-blue-800">{item.name}</span> )
+                    <span className="text-blue-800">{item.alias}</span> )
                 </h1>
             </div>
             <div className="flex items-center justify-center space-x-2 flex-wrap">
@@ -15,13 +15,13 @@ const SystemMessage = ({ item, selectField }) => {
                 {item.message?.map((field) => {
                     return (
                         <div
-                            onClick={() => selectField(field.name)}
+                            onClick={() => selectField(field.alias)}
                             className=" shadow-2xl flex items-center justify-center px-6 py-1 bg-gradient-to-r from-blue-200 to-blue-400
                                         outline-1 cursor-pointer
                                         outline-sky-500 rounded-xl text-sm font-sec space-x-2 space-x-reverse my-2 font-bold hover:outline-gray-950 trans
                                         hover:from-blue-100 "
                         >
-                            {field.name}
+                            {field.alias}
                         </div>
                     );
                 })}
